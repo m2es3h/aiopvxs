ca595022397ff95e97a763f8ce6ef7104c24aea7
Mike Smith <m2es3h@gmail.com>
2025-09-28 00:11:40 -0700

    fix exception handling (must pass python exception class), use future callback to cancel pvxs operation

--- src/client.cpp
+++ src/client.cpp
@@ -13,6 +13,8 @@ void create_submodule_client(py::module_& m) {
     using namespace pvxs;
     using namespace pvxs::client;
 
+    py::register_exception<RemoteError>(m, "RemoteError", PyExc_RuntimeError);
+
     py::class_<Operation, py::smart_holder>(m, "Operation")
         .def("name", &Operation::name)
         .def("cancel", &Operation::cancel);
@@ -36,17 +38,25 @@ void create_submodule_client(py::module_& m) {
                             })
                         );
                     }
-                    catch (const std::exception exc_info) {
+                    catch (const std::runtime_error& exc_info) {
                         py::gil_scoped_acquire lock;
                         loop.attr("call_soon_threadsafe")(
                             py::cpp_function([py_future, exc_info]() {
-                                py_future.attr("set_exception")(exc_info);
+                                py::object py_exc_type = py::module_::import("aiopvxs").attr("client").attr("RemoteError");
+                                py::object py_exc = py_exc_type(exc_info.what());
+                                py_future.attr("set_exception")(py_exc);
                             })
                         );
                     }
                 });
 
-            py_future.attr("pvxs_operation") = op_builder.exec();
+            auto op = op_builder.exec();
+            py_future.attr("add_done_callback")(
+                py::cpp_function([op](py::object py_future) {
+                    if (py_future.attr("cancelled")())
+                        op->cancel();
+                })
+            );
             return py_future;
         });
 
