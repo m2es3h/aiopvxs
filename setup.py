1c7443db1eea987beea6625e5e7d28bd3f96fa0c
Mike Smith <m2es3h@gmail.com>
2026-06-21 16:00:50 -0700

    include python site paths so it works with editable install

--- setup.py
+++ setup.py
@@ -9,8 +9,14 @@ from setuptools import find_packages, setup
 
 # get paths to pvxslibs and epicscore libraries DSOs
 compiletime_dirs = [*pvxslibs.__path__, *epicscorelibs.__path__]
-runtime_dirs = [] if sys.platform == "win32" else ["@loader_path"] if sys.platform == "darwin" else ["$ORIGIN"]
-extra_compile_args=['-D_GLIBCXX_USE_CXX11_ABI=0'] if sys.platform.startswith("linux") else []
+extra_compile_args = ['-D_GLIBCXX_USE_CXX11_ABI=0'] if sys.platform.startswith("linux") else []
+
+if sys.platform == "win32":
+    runtime_dirs = []  # os.add_dll_directory() in __init__.py used for windows
+elif sys.platform == "darwin":
+    runtime_dirs = ["@loader_path/../", getusersitepackages(), *getsitepackages()]
+else:
+    runtime_dirs = ["$ORIGIN/../", getusersitepackages(), *getsitepackages()]
 
 # declare pybind11 extension
 ext_modules = [
@@ -33,8 +39,8 @@ ext_modules = [
             str(Path(mod_dir) / "lib") for mod_dir in compiletime_dirs
         ],
         runtime_library_dirs=[
-            *[str(Path(base_dir) / ".." / "pvxslibs" / "lib") for base_dir in runtime_dirs],
-            *[str(Path(base_dir) / ".." / "epicscorelibs" / "lib") for base_dir in runtime_dirs],
+            *[str(Path(base_dir) / "pvxslibs" / "lib") for base_dir in runtime_dirs],
+            *[str(Path(base_dir) / "epicscorelibs" / "lib") for base_dir in runtime_dirs],
         ],
         libraries=["pvxs", "event_core", "Com"],
         language='c++',
